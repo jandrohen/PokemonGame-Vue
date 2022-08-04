cdbe56acf666076db3923e18a9329ff49aa5909a
Jandronit <jandrohen@gmail.com>
2022-08-04 13:04:05 +0200

    (lesson 88 course vue) Project skeleton and components

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>¿Quien es este Pokemon?</h1>
+  <PokemonPicture></PokemonPicture>
+  <PokemonOptions></PokemonOptions>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture';
+import PokemonOptions from '@/components/PokemonOptions';
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }
+
+}
+</script>
