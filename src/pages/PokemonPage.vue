2f21c9c292f395d9575329dc8c04da5a191b0183
Jandronit <jandrohen@gmail.com>
2022-08-04 13:46:59 +0200

    (lesson 88-91 course vue) Logic for get name of pokemons and functionality of PokemonPicture

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,13 +1,15 @@
 <template>
   <h1>¿Quien es este Pokemon?</h1>
-  <PokemonPicture></PokemonPicture>
-  <PokemonOptions></PokemonOptions>
+  <PokemonPicture :pokemonId="4" :showPokemon="true"/>
+  <PokemonOptions/>
 </template>
 
 <script>
 import PokemonPicture from '@/components/PokemonPicture';
 import PokemonOptions from '@/components/PokemonOptions';
+import getPokemonOptions from '@/helpers/getPokemonOptions';
 
+console.log(getPokemonOptions());
 export default {
   name: "PokemonPage",
   components: {
