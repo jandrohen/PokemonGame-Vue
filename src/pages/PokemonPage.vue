9b1637c610679a9942423b5b9ff97f122c942a80
Jandronit <jandrohen@gmail.com>
2022-08-09 21:19:46 +0200

    (lesson 94 course vue) Selected a pokemon random

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>¿Quien es este Pokemon?</h1>
-  <PokemonPicture :pokemonId="4" :showPokemon="true"/>
-  <PokemonOptions :pokemons="pokemonArr"/>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+    <h1>¿Quien es este Pokemon?</h1>
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon"/>
+    <PokemonOptions :pokemons="pokemonArr"/>
+  </div>
 
 </template>
 
@@ -19,12 +22,17 @@ export default {
   },
   data(){
     return {
-      pokemonArr: []
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false
     }
   },
   methods:{
     async mixPokemonArray(){
       this.pokemonArr = await getPokemonOptions();
+
+      const rndInt = Math.floor(Math.random() * this.pokemonArr.length);
+      this.pokemon = this.pokemonArr[rndInt];
     }
   },
   mounted() {
