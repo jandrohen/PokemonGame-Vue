90a70f5d15791392f0acc18632e07f70de8b17be
Jandronit <jandrohen@gmail.com>
2022-08-31 12:11:40 +0200

    (lesson 108 course vue) tests with emitters of vue

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -19,4 +19,29 @@ describe("Pokemon ", () => {
     expect(wrapper.html()).toMatchSnapshot();
 
   });
+
+  test("debe de mostrar las 4 opciones correctamente", () => {
+    const liTags = wrapper.findAll("li");
+    expect(liTags.length).toBe(4);
+
+    expect(liTags[0].text()).toBe("pikachu");
+    expect(liTags[1].text()).toBe("ivysaur");
+    expect(liTags[2].text()).toBe("venusaur");
+    expect(liTags[3].text()).toBe("charmander");
+  });
+
+  test("debe de emitir 'selection' con sus respectivos parámetros al hacer click", () => {
+
+    const [li1,li2,li3, li4] = wrapper.findAll("li");
+    li1.trigger("click");
+    li2.trigger("click");
+    li3.trigger("click");
+    li4.trigger("click");
+
+    expect(wrapper.emitted('selectionPokemon').length).toBe(4);
+    expect(wrapper.emitted('selectionPokemon')[0] ).toEqual([5]);
+    expect(wrapper.emitted('selectionPokemon')[1] ).toEqual([10]);
+    expect(wrapper.emitted('selectionPokemon')[2] ).toEqual([15]);
+    expect(wrapper.emitted('selectionPokemon')[3] ).toEqual([20]);
+  });
 });
