1ac42e51647fe6aefb6502d2073604a77779fe04
Jandronit <jandrohen@gmail.com>
2022-08-31 12:33:11 +0200

    (lesson 109 course vue) tests in PokemonPage

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -21,7 +21,6 @@ import PokemonPicture from '@/components/PokemonPicture';
 import PokemonOptions from '@/components/PokemonOptions';
 import getPokemonOptions from '@/helpers/getPokemonOptions';
 
-console.log(getPokemonOptions());
 export default {
   name: "PokemonPage",
   components: {
